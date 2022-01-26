423c068aa23db400040ea30298d6376e64d07522
YimKeul <leesjun29@naver.com>
2022-01-27 01:56:11 +0900

    test

--- src/screens/Result.js
+++ src/screens/Result.js
@@ -18,7 +18,7 @@ import { AntDesign } from "@expo/vector-icons";
 import DropShadow from "react-native-drop-shadow";
 import { firebase_db } from "../firebaseConfig";
 import * as Application from "expo-application";
-//test 3
+//test 4
 export default function Result({ navigation, route }) {
   LogBox.ignoreAllLogs();
 
